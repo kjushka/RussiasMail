cd6e7ed87824cef795f3cc23f8b50f691a6ebd52
petrov_anton <petrusha230@gmail.com>
2020-09-26 01:46:13 +0800

    First

---
+++ mail/src/main/java/model/Service.java
@@ -0,0 +1,25 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "service_type")
+    private ServiceType serviceType;
+}
---
+++ mail/src/main/java/model/ServiceType.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+public class ServiceType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+}
