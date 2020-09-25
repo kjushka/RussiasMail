34b2eb90b982308f9f1bdfa855c5aa90e206bb34
Casper27-th <Casper227799@gmail.com>
2020-09-26 01:05:45 +0800

    log

---
+++ mail/src/main/java/model/executor.java
@@ -0,0 +1,29 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "executors")
+/* Базовый класс исполнителя*/
+public class executor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "timeSlots")
+    private String timeSlots;
+    @Column(name = "deliveryTime")
+    private Integer dTime;
+    @Column(name = "capacity")
+    private Integer capacity;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "deliveryType_id", referencedColumnName = "id")
+    private TypeDelivery typeD;
+}
