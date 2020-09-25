a9285d92ad28be191c205f30ae3bfab006c2c75b
petrov_anton <petrusha230@gmail.com>
2020-09-25 21:43:14 +0800

    create project

---
+++ mail/src/main/java/model/GeoZone.java
@@ -0,0 +1,21 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "geozone")
+public class GeoZone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+}
