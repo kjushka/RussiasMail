8ea3c3801e4119ffc444b572c60e29bebe93fd2c
petrov_anton <petrusha230@gmail.com>
2020-09-26 13:03:24 +0800

    Check CustomGeozone

--- mail/src/main/java/ru/isu/controller/HomeController.java
+++ mail/src/main/java/ru/isu/controller/HomeController.java
@@ -1,12 +1,35 @@
 package ru.isu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.isu.model.Geozone;
+import ru.isu.repository.GeozoneRepository;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private GeozoneRepository geozoneRepository;
+
     @RequestMapping(value = "/")
     public String home() {
         return "home";
+    }
+
+    @RequestMapping(value = "/form")
+    public String createForm(Model model) {
+        List<Geozone> geozones = geozoneRepository.findAll();
+        model.addAttribute("geozones", geozones);
+        return "form";
+    }
+
+    @RequestMapping(value = "/makeorder", method = RequestMethod.POST)
+    public String makeOrder() {
+        System.out.println("Sosi");
+        return "redirect: home";
     }
 }
